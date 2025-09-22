26551610df6ed20a5eb19a2b13e16b1b8c905da5
Fernando Giorgetti <fgiorget@redhat.com>
2025-09-22 12:16:36 -0300

    Setting _ANSIBLE_PROFILE to prevent unit tests from failing

--- tests/unit/utils/ansible_module_mock.py
+++ tests/unit/utils/ansible_module_mock.py
@@ -9,6 +9,7 @@ def set_module_args(args):
     """prepare arguments so that they will be picked up during module creation"""
     args = json.dumps({'ANSIBLE_MODULE_ARGS': args})
     basic._ANSIBLE_ARGS = to_bytes(args)
+    basic._ANSIBLE_PROFILE = 'legacy'
 
 
 class AnsibleExitJson(Exception):

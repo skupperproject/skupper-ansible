8ef26723d9e444caa35df95cd34f4b7a15f8c039
Fernando Giorgetti <fgiorget@redhat.com>
2025-10-28 16:27:19 -0300

    Fix unhandled exception when a running a command that does not exist

--- plugins/module_utils/command.py
+++ plugins/module_utils/command.py
@@ -7,9 +7,13 @@ from ansible.module_utils.basic import AnsibleModule
 
 
 def run_command(module: AnsibleModule, command: list) -> t.Tuple[int, str, str]:
-    module.debug("running command: %s" % (command))
-    code, out, err = module.run_command(command)
-    module.debug("code: %d" % (code))
-    module.debug("stdout: %s" % (out))
-    module.debug("stderr: %s" % (err))
-    return code, out, err
+    try:
+        module.debug("running command: %s" % (command))
+        code, out, err = module.run_command(command, handle_exceptions=False)
+        module.debug("code: %d" % (code))
+        module.debug("stdout: %s" % (out))
+        module.debug("stderr: %s" % (err))
+        return code, out, err
+    except Exception as ex:
+        module.debug("exception running command %s: %s" % (command, str(ex)))
+        return 1, "", str(ex)
